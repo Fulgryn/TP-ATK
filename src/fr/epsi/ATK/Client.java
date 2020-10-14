d1ebedbd3fa90ccdfcadc4298490c4fca4cb0530
Gabin <gabdu74@gmail.com>
2020-10-14 19:01:17 +0200

    Creation Database + insertion ✓

--- src/fr/epsi/ATK/Client.java
+++ src/fr/epsi/ATK/Client.java
@@ -1,7 +1,8 @@
 package fr.epsi.ATK;
+import java.sql.*;
 
 /**
- *
+ * Classe client stockant tous les clients pour les inserer facilement en base de donnée
  */
 public class Client {
     String id;
@@ -13,11 +14,11 @@ public class Client {
     }
 
     /**
-     *
-     * @param id
-     * @param prenom
-     * @param nom
-     * @param mail
+     * Constructeur de la classe client comprenant tous les paramètres.
+     * @param id id du client
+     * @param prenom prénom du client
+     * @param nom nom du iencli
+     * @param mail mail du client
      */
     public Client(String id, String prenom, String nom, String mail) {
         this.id = id;
@@ -34,5 +35,33 @@ public class Client {
                 ", prenom='" + prenom + '\'' +
                 ", mail='" + mail + '\'' +
                 '}';
+    }
+
+    void insertClient(){
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager
+                    .getConnection("jdbc:postgresql://localhost:5432/ATK",
+                            "postgres", "admin");
+            c.setAutoCommit(false);
+            System.out.println("Opened database successfully");
+
+            stmt = c.createStatement();
+            String sql = "INSERT INTO CLIENT (ID,PRENOM,NOM,MAIL) "
+                    + "VALUES ('" + this.id +"', '"+ this.prenom +"', '"+ this.nom +"', '"+ this.mail +"');";
+
+            System.out.println(sql);
+            stmt.executeUpdate(sql);
+
+            stmt.close();
+            c.commit();
+            c.close();
+        } catch (Exception e) {
+            System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Records created successfully");
     }
 }
--- src/fr/epsi/ATK/Main.java
+++ src/fr/epsi/ATK/Main.java
@@ -1,17 +1,67 @@
 package fr.epsi.ATK;
 
+import java.sql.Statement;
 import java.util.*;
 import java.io.*;
 import java.lang.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 
 public class Main{
     public static void main(String[] args) throws FileNotFoundException{
         System.out.println("démarrage...");
 
-        //System.out.println(new File(".").getAbsoluteFile());
-        Scanner scanner = new Scanner(new File("E:/IdeaProjects/lab0/client.txt"));
+        //connexion à la DB
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager
+                    .getConnection("jdbc:postgresql://localhost:5432/ATK",
+                            "postgres", "admin");
 
+            System.out.println("Opened database successfully");
+            stmt = c.createStatement();
+            String sql = "CREATE TABLE CLIENT " +
+                    "(ID             CHAR(3) PRIMARY KEY     NOT NULL," +
+                    " PRENOM         VARCHAR(20)    NOT NULL, " +
+                    " NOM            VARCHAR(30)    NOT NULL, " +
+                    " MAIL           VARCHAR(50)    NOT NULL) ";
+            stmt.executeUpdate(sql);
+            stmt.close();
+            c.close();
+            /*-- Table: public.client
+
+                    -- DROP TABLE public.client;
+
+            CREATE TABLE public.client
+                    (
+                            prenom character varying(20) COLLATE pg_catalog."default",
+                            nom character varying(30) COLLATE pg_catalog."default",
+                            id character(3) COLLATE pg_catalog."default" NOT NULL,
+                            mail character varying(60) COLLATE pg_catalog."default",
+                            CONSTRAINT "Client_pkey" PRIMARY KEY (id)
+                    )
+
+            TABLESPACE pg_default;
+
+            ALTER TABLE public.client
+            OWNER to postgres;*/
+
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("database created successfully");
+
+
+        //System.out.println(new File(".").getAbsoluteFile());
+        Scanner scanner = new Scanner(new File("E:/IdeaProjects/ATK/src/fr/epsi/ATK/client.txt"));
         while(scanner.hasNext()){
             String[] tokens = scanner.nextLine().split(";");
             String id = tokens[0];
@@ -20,9 +70,10 @@ public class Main{
             String mail = tokens[3];
 
             Client client = new Client(id, prenom, nom, mail);
-
+            client.insertClient();
             System.out.println(client.toString());
-        }
+    }
+
 
     }
 }
